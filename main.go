77c3ebd457f4bdcc0515c94602a0003c8ba4ffc5
G <g@devnull.rip>
2026-08-27 00:35:02 -0700

    healthz (#7)

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 )
 
+func healthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/", fs)
+	fsClean := http.StripPrefix("/app/", fs)
+	mux.Handle("/app/", fsClean)
+	mux.HandleFunc("/healthz", healthz)
 
 	server := &http.Server{
 		Addr:    ":8080",
