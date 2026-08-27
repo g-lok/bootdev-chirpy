d25f96468d4dc905d6eba6bbe8b4986a0284a34c
G <g@devnull.rip>
2026-08-27 01:25:11 -0700

    stateful handlers (#8)

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
+func (cfg *apiConfig) getVisits(w http.ResponseWriter, req *http.Request) {
+	hits := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) resetVisits(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Visits counter reset"))
+}
 
 func healthz(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -12,11 +39,14 @@ func healthz(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./static"))
 	fsClean := http.StripPrefix("/app/", fs)
-	mux.Handle("/app/", fsClean)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fsClean))
 	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/metrics", apiCfg.getVisits)
+	mux.HandleFunc("/reset", apiCfg.resetVisits)
 
 	server := &http.Server{
 		Addr:    ":8080",
